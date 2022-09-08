b7872b91c1f13b29a3e56f1c838a17805a3e6783
sami_mengistu <samialxprogram@gmail.com>
2022-09-08 08:40:09 -0700

    a C program that prints exactly (Programming is like building a multilingual puzzle)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 int main(void)
 {
 /**
-*main - main-block
-*Return : 0
-*/
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
